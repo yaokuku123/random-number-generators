65b171e325c9dba125ef44f3f1dd26c63d645307
yorick <34773171+yaokuku123@users.noreply.github.com>
2020-01-09 16:46:41 +0800

    Add files via upload

---
+++ OBJ/md5.d
@@ -0,0 +1,5 @@
+..\obj\md5.o: ..\HARDWARE\HASH\md5.c
+..\obj\md5.o: ..\HARDWARE\HASH\md5.h
+..\obj\md5.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdio.h
+..\obj\md5.o: D:\MDK5\ARM\ARMCC\Bin\..\include\string.h
+..\obj\md5.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdlib.h
---
+++ OBJ/sha256.d
@@ -0,0 +1,4 @@
+..\obj\sha256.o: ..\HARDWARE\HASH\sha256.c
+..\obj\sha256.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdio.h
+..\obj\sha256.o: D:\MDK5\ARM\ARMCC\Bin\..\include\stdlib.h
+..\obj\sha256.o: ..\HARDWARE\HASH\sha256.h
